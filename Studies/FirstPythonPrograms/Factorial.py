a390c89965eb707531339f509b941db49d38f85b
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-03-31 22:31:44 -0300

    Factorial

---
+++ Studies/FirstPythonPrograms/Factorial.py
@@ -0,0 +1,13 @@
+try:
+    number = int(input('Digite um número: '))
+
+    number_factorial = 1
+
+    for i in range(1, number + 1):
+        number_factorial *= i
+
+    print("O fatorial de", number, "é:", number_factorial)
+
+except:
+    print('ERRO')
+
