7ff167bff08cd7391d6857a1a4bbcb3ef50762ce
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-03-31 21:47:12 -0300

    Odd and even check

---
+++ Studies/FirstPythonPrograms/NumberCheck.py
@@ -0,0 +1,13 @@
+if __name__ == ' __main__':
+    pass
+
+try:
+    number = int(input('Enter a number\n'))
+
+    if number % 2 == 0:
+        print('EVEN NUMBER')
+    else:
+        print('ODD NUMBER')
+
+except:
+    print('ERRO')
